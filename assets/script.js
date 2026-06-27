5716e26315817fd0bcabc02a9f8dffc81c1af7ba
Julien Ozarowski <julien.ozarowski@decathlon.com>
2026-06-27 03:04:30 +0200

    adding js for carousel

--- assets/script.js
+++ assets/script.js
@@ -17,24 +17,41 @@ const slides = [
 	}
 ]
 
-const indexSlide = 0
+let indexSlide = 0
 
-function backSlide() {
+document.querySelector(".arrow_left").addEventListener("click", function() {
+	if (indexSlide === 0) {
+		indexSlide = slides.length - 1
+	} else {
+		indexSlide--
+	}
+	updateSlide()
+})
 
-}
+document.querySelector(".arrow_right").addEventListener("click", function() {
+	if (indexSlide === slides.length - 1) {
+		indexSlide = 0
+	} else {
+		indexSlide++
+	}
+	updateSlide()
+})
 
-function forwardSlide() {
-
+function updateSlide() {
+	document.querySelector(".banner-img").src = `assets/images/slideshow/${slides[indexSlide].image}`
+	document.querySelector("#banner p").innerHTML = slides[indexSlide].tagLine
+	updateDots()
 }
 
 function displayDots() {
-
 }
 
 function updateDots() {
-
+	const dots = document.querySelectorAll(".dot")
+	dots.forEach(function(dot) {
+		dot.classList.remove("dot_selected")
+	})
+	dots[indexSlide].classList.add("dot_selected")
 }
 
-function displayDots() {
-
-}
+updateSlide()
